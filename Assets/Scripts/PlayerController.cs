65da4bef42e37c2be52f50abea6daf255775aff8
James Walsh <jamesandersonwalsh@gmail.com>
2023-04-08 23:37:54 -0600

    Adjust controllers for ISO camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float playerMovementSpeed = 5f;
+    [SerializeField] float playerRotationSpeed = 500;
 
     // Update is called once per frame
     void Update()
     {
-        var horizontalInput = Input.GetAxis("Horizontal");
-        var verticalInput = Input.GetAxis("Vertical");
+        Vector3 isometricInputDirection = GetIsometricControllerInput();
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
+        MovePlayer(isometricInputDirection * Time.deltaTime * playerMovementSpeed);
+        RoatatePlayer(isometricInputDirection);
+    }
 
-        var movementSpeedPerFrame = movementDirection * Time.deltaTime * playerMovementSpeed;
-        transform.Translate(movementSpeedPerFrame, Space.World);
+    private Vector3 GetIsometricControllerInput() {
+        var controllerInput = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            0,
+            Input.GetAxisRaw("Vertical")
+        );
 
-        // TODO: Im not sure what this does yet so I should research it.
-        if (movementDirection != Vector3.zero) {
-            transform.forward =  movementDirection;
+        Matrix4x4 matrixAdjusted45Degrees = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+        Vector3 skewedIsometricInputDirection = matrixAdjusted45Degrees.MultiplyPoint3x4(controllerInput);
+
+        skewedIsometricInputDirection.Normalize();
+
+        return skewedIsometricInputDirection;
+    }
+
+    private void MovePlayer(Vector3 movementPerFrame) {
+        transform.Translate(movementPerFrame, Space.World);
+    }
+
+    private void RoatatePlayer(Vector3 inputDirection) {
+         if (inputDirection != Vector3.zero) {
+            var relativeForwardDirection = LookForwardDirection(inputDirection);
+            Quaternion toRotation = Quaternion.LookRotation(relativeForwardDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, playerRotationSpeed * Time.deltaTime);
         }
+    }
+
+    private Vector3 LookForwardDirection(Vector3 isometricInputDirection) {
+        return (transform.position + isometricInputDirection) - transform.position;
     }
 }
