45dd206cf7c85c44b670d644a36698815591cee5
James Walsh <jamesandersonwalsh@gmail.com>
2023-04-08 22:49:22 -0600

    Rotate player model while turning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float playerMovementSpeed = 5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        var horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * playerMovementSpeed;
-        var verticalInput = Input.GetAxis("Vertical") * Time.deltaTime * playerMovementSpeed;
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(horizontalInput, 0, verticalInput);
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+        var movementSpeedPerFrame = movementDirection * Time.deltaTime * playerMovementSpeed;
+        transform.Translate(movementSpeedPerFrame, Space.World);
+
+        // TODO: Im not sure what this does yet so I should research it.
+        if (movementDirection != Vector3.zero) {
+            transform.forward =  movementDirection;
+        }
     }
 }
